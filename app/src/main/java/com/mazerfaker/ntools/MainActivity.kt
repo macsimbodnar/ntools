8a750b78e8f52c3665533bb6cf9d9ce7cf3c6d87
Max <max@keyless.io>
2020-10-22 17:09:09 +0200

    First commit

---
+++ app/src/main/java/com/mazerfaker/ntools/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.mazerfaker.ntools
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
